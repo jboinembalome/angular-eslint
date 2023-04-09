e68bbc11ae551a93e5c9988250675aa844a91a39
Jimmy Boinembalome <jboinembalome@gmail.com>
2023-04-09 13:13:32 +0200

    Show hello world message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-eslint';
+  helloWorld?: string;
+
+  ngOnInit(): void {
+    this.helloWorld = this.getHelloWorld();
+  }
+
+  private getHelloWorld(): string {
+    let hello = 'Hello';
+    const world = 'World';
+
+    return `${hello} ${world}!`;
+  }
 }
